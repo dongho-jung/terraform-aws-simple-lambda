10ec25f293945c59fb7da6dd89538f0b2985e4bd
dongho-jung <dongho971220@gmail.com>
2026-01-01 08:16:52 +0900

    chore: update terraform provider versions
    - Provider 버전 업데이트
      - AWS provider 5.82.2 → 6.27.0
      - Docker provider 3.0.2 → ~> 3.9.0
    - Docker provider source 변경
      - kreuzwerker/docker → registry.opentofu.org/kreuzwerker/docker

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.82.2"
+      version = "6.27.0"
     }
     docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      source = "registry.opentofu.org/kreuzwerker/docker"
+      version = "~> 3.9.0"
     }
     null = {
       source  = "hashicorp/null"
